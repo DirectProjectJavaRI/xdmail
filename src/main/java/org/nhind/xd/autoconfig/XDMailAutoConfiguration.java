3ecacac8349a1ff80340d54d84144a1d6c47446f
greg.meyer <greg.meyer@oracle.com>
2025-08-07 18:18:56 -0500

    Initial RI 9.0 commit.

--- src/main/java/org/nhind/xd/autoconfig/XDMailAutoConfiguration.java
+++ src/main/java/org/nhind/xd/autoconfig/XDMailAutoConfiguration.java
@@ -1,8 +1,10 @@
-package org.nhind.xd.springconfig;
+package org.nhind.xd.autoconfig;
 
 import org.nhind.mail.service.DocumentRepository;
 import org.nhind.mail.service.XDDeliveryCallback;
 import org.nhind.mail.service.XDDeliveryCore;
+import org.nhind.xd.streams.processors.DefaultXDDeliveryCallback;
+import org.nhind.xd.streams.processors.XDRemoteDeliveryProcessor;
 import org.nhindirect.common.tx.impl.DefaultTxDetailParser;
 import org.nhindirect.gateway.smtp.NotificationSettings;
 import org.nhindirect.gateway.smtp.ReliableDispatchedNotificationProducer;
@@ -10,12 +12,14 @@ import org.nhindirect.xd.routing.RoutingResolver;
 import org.nhindirect.xd.transform.impl.DefaultMimeXdsTransformer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
-@Configuration
-public class XDDeliveryConfig
+@AutoConfiguration
+@Import(XDRemoteDeliveryProcessor.class)
+public class XDMailAutoConfiguration
 {
 	@Value("direct.gateway.xd.endpointUrl")
 	protected String endpointURL;
@@ -23,12 +27,17 @@ public class XDDeliveryConfig
 	@Autowired
 	protected RoutingResolver resolver;
 	
-	@Autowired
-	protected XDDeliveryCallback xdDeliveryCallback;
+	@Bean
+	@ConditionalOnMissingBean
+    XDDeliveryCallback xdDeliveryCallback() {
+		
+		return new DefaultXDDeliveryCallback();
+		
+	}
 	
 	@Bean
 	@ConditionalOnMissingBean
-	public XDDeliveryCore xdDeliveryCore()
+	XDDeliveryCore xdDeliveryCore(XDDeliveryCallback xdDeliveryCallback)
 	{
 		final ReliableDispatchedNotificationProducer notificationProducer = 
 				new ReliableDispatchedNotificationProducer(new NotificationSettings(true, "Direct XD Delivery Agent", "Your message was successfully dispatched."));
